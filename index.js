9eae04a4299624fef3fe0837868d8b92a4cc2d08
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-09 18:50:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,11 @@
     return "No, I must disagree";
   }
 
+<<<<<<< HEAD
     BoardMember.prototype.approve  =  function (){
+=======
+    BoardMember.prototype.veto =  function (){
+>>>>>>> f49eded350ba0851895ee311df6c2a33326fd08f
     return "You can do that!";
   }
 
@@ -22,5 +26,9 @@
   }
   
     BoardMember.prototype.sayHi =  function (){
+<<<<<<< HEAD
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+=======
+    return "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.";
+>>>>>>> f49eded350ba0851895ee311df6c2a33326fd08f
   }
